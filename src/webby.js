e2a9ab1eab22e7a9815efc951f90ba4285c22bb6
Anthony Ramirez <ar4477@nyu.edu>
2018-10-09 11:35:58 -0400

    fix HTTP Status Codes values and fix/finish App and Response classes

--- src/webby.js
+++ src/webby.js
@@ -4,8 +4,8 @@ const net = require('net');
 
 const HTTP_STATUS_CODES = {
   200: 'OK',
-  404: 'NOT FOUND',
-  500: 'INTERNAL SERVER ERROR'
+  404: 'Not Found',
+  500: 'Internal Server Error'
 }
 
 const MIME_TYPES = {
@@ -70,21 +70,21 @@ class App {
         // counter for remainder of string
         let j = i + 1;
         // iterate through rest of string until ?, #, or = is encountered
-          while (j < path.length - 1) {
-            if (path.charAt(j) !== '?' && path.charAt(j) !== '#' && path.charAt(j) !== '=') {
-              // add next letter assuming it is valid or else break out of while loop
-              pathToCheck += path.charAt(j);
-              j++;
-            }
-            else{
-              break;
-            }
+        while (j < path.length - 1) {
+          if (path.charAt(j) !== '?' && path.charAt(j) !== '#' && path.charAt(j) !== '=') {
+            // add next letter assuming it is valid or else break out of while loop
+            pathToCheck += path.charAt(j);
+            j++;
           }
-          // if the remainder of the valid string is not empty,
-          // there is more to the path so add another '/' to normPath
-          if (pathToCheck !== '') {
-            normPath += '/';
+          else{
+            break;
           }
+        }
+        // if the remainder of the valid string is not empty,
+        // there is more to the path so add another '/' to normPath
+        if (pathToCheck !== '') {
+          normPath += '/';
+        }
       }
       i++;
     }
@@ -117,38 +117,111 @@ class App {
 
   handleConnection(sock) {
     // set the callback for the socket's on method
-    sock.on('data', sock=> this.handleRequest(sock));
+    sock.on('data', (binaryData) => this.handleRequest(sock, binaryData));
   }
 
   handleRequest(sock, binaryData) {
-    // TODO
+    // create new request and response objects, using binaryData string and sock, respectively
+    let newRequest = new Request('' + binaryData);
+    let newResponse = new Response(sock, 200, 'HTTP/1.1');
+    // if middleware is set, use it with the new request and
+    // response objects and use processRoutes as the next function
+    if (this.middleware !== null) {
+      this.middleware(newRequest, newResponse, this.processRoutes(newRequest, newResponse));
+    }
+    else {
+      // if middleware is not set, just call processRoutes
+      this.processRoutes(newRequest, newResponse)
+    }
   }
 
   processRoutes(req, res) {
-    // TODO
-  }
+    // get function stored in routes, based on request's method and path
+      const f = this.routes[this.createRouteKey(req.method, req.path)];
 
+      // if there is the function exists, call it, or else send 404 page
+      if (f) {
+        f(req, res);
+      }
+      else {
+        res.status(404).send("Page not found");
+      }
+  }
 }
 
 class Response {
 
   constructor(socket, statusCode, version) {
+    // set properties
     this.sock = socket;
     // check if statusCode and version are set, and if they're not, use default values
-    if (statusCode) {
-      this.statucCode = statusCode;
+    if (statusCode !==  undefined) {
+      this.statusCode = statusCode;
     }
     else {
       this.statusCode = 200;
     }
-    if (version) {
+    if (version !== undefined) {
       this.version = version;
     }
     else {
       this.version = 'HTTP/1.1';
     }
+
+    this.headers = {};
+    this.body = '';
   }
 
+  set(name, value) {
+    // add header and header values
+    this.headers[name] = value;
+  }
+
+  end() {
+    // call end on socket
+    this.sock.end();
+  }
+
+  statusLineToString() {
+    // return HTTP version, status code, and status code message
+    return this.version + ' ' + this.statusCode + ' ' + HTTP_STATUS_CODES[this.statusCode] + '\r\n';
+  }
+
+  headersToString() {
+    let s = ''
+    // add headers and values follwed by line and feed return to string
+    for (const h in this.headers) {
+      s += h;
+      s += ': ' + this.headers[h];
+      s += '\r\n';
+    }
+
+    // return string
+    return s;
+  }
+
+  send(body) {
+    // set content-type to html, even if header doesn't exist
+    this.set('Content-Type', 'text/html');
+    // TODO check if text/html is in headers
+    // write headers and extra line break
+    this.sock.write(this.statusLineToString());
+    this.sock.write(this.headersToString());
+    this.sock.write('\r\n');
+    // write body and end connection
+    this.sock.write(body);
+    this.sock.end();
+  }
+
+  status(statusCode) {
+    // set status code and return Response object
+    this.statusCode = statusCode;
+    return this;
+  }
+}
+
+function serveStatic() {
+  // TODO
 }
 
 module.exports = {
